3fd0649c8b19a6eedbb636d88d8af2ca98929166
diegotestereo <diegotestereo@gmail.com>
2014-06-06 19:40:14 -0300

    casa

--- RButton_CkBox/src/com/example/rbutton_ckbox/MainActivity.java
+++ RButton_CkBox/src/com/example/rbutton_ckbox/MainActivity.java
@@ -1,25 +1,54 @@
 package com.example.rbutton_ckbox;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Toast;
 public class MainActivity extends Activity {
 
+	private CheckBox chBox1,chBox2,chBox3;
+    private TextView txtV_Check;
+   
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_main);
-
-       
+        levantarXML();
+        //  setup();
+  pepe();
     }
+
+ 
+    
+    
+    
+	private void setup() {
+		chBox1.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		
+				if (isChecked){
+					txtV_Check.setText("check 1 tildado");
+					
+				}else txtV_Check.setText("check 1 destildado");
+		
+			}
+		});
+	}
+
+	private void levantarXML() {
+		chBox1=(CheckBox) findViewById(R.id.checkBox1);
+		chBox2=(CheckBox) findViewById(R.id.checkBox2);
+		chBox3=(CheckBox) findViewById(R.id.checkBox3);
+		txtV_Check=(TextView) findViewById(R.id.txtV_Check);
+	}
 
 
    
