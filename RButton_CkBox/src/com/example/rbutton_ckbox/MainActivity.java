fb5c32868019d17114bb39a76abeafe4e5830a1a
diegotestereo <diegotestereo@gmail.com>
2014-06-08 22:55:12 -0300

    casa

--- RButton_CkBox/src/com/example/rbutton_ckbox/MainActivity.java
+++ RButton_CkBox/src/com/example/rbutton_ckbox/MainActivity.java
@@ -2,16 +2,17 @@ package com.example.rbutton_ckbox;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.Toast;
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity  {
 
 	private CheckBox chBox1,chBox2,chBox3;
-    private TextView txtV_Check;
+    private TextView txtV_Check,txtV_seekbar;
+    private SeekBar seekBar1;
    
     
     @Override
@@ -21,13 +22,67 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         levantarXML();
         //  setup();
-  pepe();
+        pepe();
+        seekBar1.setProgress(0);
+          barra();      
     }
 
  
     
     
     
+	
+
+
+
+
+
+	private void barra() {
+		
+		
+		seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				
+				txtV_seekbar.setText("Señal: "+progress+" dbm");
+				
+			}
+		});
+	}
+
+
+
+
+
+
+
+
+
+
+
+	private void pepe() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
 	private void setup() {
 		chBox1.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			
@@ -48,6 +103,9 @@ public class MainActivity extends Activity {
 		chBox2=(CheckBox) findViewById(R.id.checkBox2);
 		chBox3=(CheckBox) findViewById(R.id.checkBox3);
 		txtV_Check=(TextView) findViewById(R.id.txtV_Check);
+		seekBar1=(SeekBar) findViewById(R.id.seekBar1);
+		txtV_seekbar=(TextView) findViewById(R.id.txtV_Seek);
+		txtV_seekbar.setText("Señal: 0 dbm");
 	}
 
 
